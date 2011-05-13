d868f956f4399f5c77997fed84e7fcf0611b0e83
Rene Milk <rene.milk@uni-muenster.de>
2011-05-13 17:37:26 +0200

    fix run script

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
 cd $(dirname $0)
-export PYTHONPATH=$(pwd):$(pwd)/encutils:$(pwd)/cssutils:$(pwd)/svg.charts:$PYTHONPATH
-while [ 1 ] ; do
-	python Main.py # | tee out.log 
-	sleep 2
-done
+nice python main.py
